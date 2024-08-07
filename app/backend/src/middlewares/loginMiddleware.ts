f38a2522c39e6371bf1f3663004f00de216a7661
Joao Vitor <jvitorss2021@gmail.com>
2024-08-07 10:47:03 -0300

    alterando o middleware de login

--- app/backend/src/middlewares/loginMiddleware.ts
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -9,10 +9,10 @@ const ValidPassword = (password: string) => password.length >= 6;
 
 const loginMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (!req.body.email || !req.body.password) {
-    return res.status(400).json({ error: 'All fields must be filled' });
+    return res.status(400).json({ message: 'All fields must be filled' });
   }
   if (!ValidEmail(req.body.email) || !ValidPassword(req.body.password)) {
-    return res.status(401).json({ error: 'Invalid email or password' });
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
 
   next();
