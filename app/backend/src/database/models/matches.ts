bf95ebe91dc2d68f9098db86ada3d18ac727b0e5
Joao Vitor <jvitorss2021@gmail.com>
2024-08-08 15:04:16 -0300

    migrations e models para matches

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,49 @@
+import { Model, DataTypes,
+  InferAttributes, InferCreationAttributes, CreationOptional } from 'sequelize';
+import db from '.';
+
+class SequelizeMatches extends Model<InferAttributes<SequelizeMatches>,
+InferCreationAttributes<SequelizeMatches>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+SequelizeMatches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Matches',
+  tableName: 'matches',
+  timestamps: false,
+});
+
+export default SequelizeMatches;
