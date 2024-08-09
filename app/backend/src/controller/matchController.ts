4c22ad3b205ac5a5431d4e1e2ef445f7aaf0c1d7
Joao Vitor <jvitorss2021@gmail.com>
2024-08-09 12:05:00 -0300

    endpoint patch adicionado

--- app/backend/src/controller/matchController.ts
+++ app/backend/src/controller/matchController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import MatchService from '../service/matchService';
+import SequelizeMatches from '../database/models/matches';
 
 class MatchController {
   static getAllMatches = async (req: Request, res: Response) => {
@@ -20,5 +21,23 @@ class MatchController {
       return res.status(500).json({ message: 'Internal Server Error' });
     }
   };
+
+  static async finishMatch(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    try {
+      const match = await SequelizeMatches.findByPk(id);
+
+      if (!match) {
+        return res.status(404).json({ message: 'Match not found' });
+      }
+
+      await match.update({ inProgress: false });
+
+      return res.status(200).json({ message: 'Finished' });
+    } catch (error) {
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  }
 }
 export default MatchController;
--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import MatchController from '../controller/matchController';
+import authMiddleware from '../middlewares/authMiddleware';
 
 const matchRouter = Router();
 
 matchRouter.get('/matches', (req, res) => MatchController.getAllMatches(req, res));
+matchRouter.patch('/matches/:id/finish', authMiddleware, MatchController.finishMatch);
 
 export default matchRouter;
