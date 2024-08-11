cfb0fc7122c97da3d4d8cf49153735d84d6c91db
Joao Vitor <jvitorss2021@gmail.com>
2024-08-11 16:16:20 -0300

    testes para leaderboard

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,41 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { app } from '../app';
+import LeaderboardService from '../service/leaderboardService';
+import TeamService from '../service/TeamService';
+import MatchService from '../service/matchService';
+import { matchesMock, leaderboardMock } from './mocks/leaderboard.mock';
+import { teamsMock } from './mocks/team.mock';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('LeaderboardService and LeaderboardController', function () {
+  afterEach(() => {
+    sinon.restore();
+  });
+  describe('LeaderboardService', function () {
+    it('should return a sorted and calculated leaderboard', async function () {
+      sinon.stub(TeamService.prototype, 'getAllTeams').resolves(teamsMock);
+      sinon.stub(MatchService, 'getMatches').resolves(matchesMock);
+
+      const result = await LeaderboardService.getLeaderboard();
+
+      expect(result).to.deep.equal(leaderboardMock);
+    });
+
+    it('should call getAllTeams and getMatches with correct arguments', async function () {
+      const getAllTeamsStub = sinon.stub(TeamService.prototype, 'getAllTeams').resolves(teamsMock);
+      const getMatchesStub = sinon.stub(MatchService, 'getMatches').resolves(matchesMock);
+
+      await LeaderboardService.getLeaderboard();
+
+      sinon.assert.calledOnce(getAllTeamsStub);
+      sinon.assert.calledOnce(getMatchesStub);   
+      sinon.assert.calledWith(getMatchesStub, false); 
+    });
+  });
+
+});
---
+++ app/backend/src/tests/mocks/leaderboard.mock.ts
@@ -0,0 +1,45 @@
+export const matchesMock = [
+  {
+    id: 1,
+    homeTeamId: 1,
+    homeTeamGoals: 3,
+    awayTeamId: 2,
+    awayTeamGoals: 1,
+    inProgress: false,
+  },
+  {
+    id: 2,
+    homeTeamId: 2,
+    homeTeamGoals: 2,
+    awayTeamId: 1,
+    awayTeamGoals: 2,
+    inProgress: false,
+  },
+];
+
+export const leaderboardMock = [
+  {
+    name: 'Team A',
+    totalPoints: 3,
+    totalGames: 1,
+    totalVictories: 1,
+    totalDraws: 0,
+    totalLosses: 0,
+    goalsFavor: 3,
+    goalsOwn: 1,
+    goalsBalance: 2,
+    efficiency: '100.00%',
+  },
+  {
+    name: 'Team B',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 2,
+    goalsOwn: 2,
+    goalsBalance: 0,
+    efficiency: '33.33%',
+  },
+];
